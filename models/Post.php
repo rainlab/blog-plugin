4cb7bf6d322f28fdf75a485565f78b249d6bb09e
Sam Georges <sam@daftspunk.com>
2013-11-27 17:34:54 +1100

    This adds some useful columns to the Post list

--- models/Post.php
+++ models/Post.php
@@ -19,6 +19,10 @@ class Post extends Model
     /*
      * Relations
      */
+    public $belongsTo = [
+        'user' => ['Modules\Backend\Models\User', 'foreignKey' => 'user_id']
+    ];
+
     public $belongsToMany = [
         'categories' => ['Plugins\October\Blog\Models\Category', 'table' => 'october_blog_posts_categories']
     ];
