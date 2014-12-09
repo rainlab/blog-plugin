13c952344368d9e495b59fd32a2f04c1ee51810d
RAFIE Younes <younes.rafie@gmail.com>
2014-12-09 14:27:51 +0000

    Update required permissions
    - Added requiredPermissions to `posts` and `categories` controllers.
    - Added user_id to database table.
    - Created a new permission to allow user to manage others posts, and
    restrict user to manage only their posts.

--- controllers/Posts.php
+++ controllers/Posts.php
@@ -1,9 +1,12 @@
 <?php namespace RainLab\Blog\Controllers;
 
+use Backend\Facades\Backend;
+use Backend\Facades\BackendAuth;
 use Flash;
 use BackendMenu;
 use Backend\Classes\Controller;
 use RainLab\Blog\Models\Post;
+use System\Classes\ApplicationException;
 
 class Posts extends Controller
 {
@@ -16,6 +19,8 @@ class Posts extends Controller
     public $listConfig = 'config_list.yaml';
 
     public $bodyClass = 'compact-container';
+
+    public $requiredPermissions = ['rainlab.blog.access_other_posts', 'rainlab.blog.access_posts'];
 
     public function __construct()
     {
@@ -41,15 +46,70 @@ class Posts extends Controller
         $this->asExtension('ListController')->index();
     }
 
+    public function update($recordId, $context = null)
+    {
+        $user = BackendAuth::getUser();
+        $post = Post::find($recordId);
+
+        if (!$post->userCanEdit($user)) {
+            return \Redirect::to(Backend::url('rainlab/blog/posts'));
+        }
+
+        return $this->asExtension('FormController')->update($recordId, $context);
+
+    }
+
+    public function listExtendQueryBefore($query)
+    {
+        $user = BackendAuth::getUser();
+
+        if( !$user->hasAnyAccess(['rainlab.blog.access_other_posts']) ) {
+            $query->where('user_id', '=', $user->id);
+        }
+    }
+
     public function index_onDelete()
     {
         if (($checkedIds = post('checked')) && is_array($checkedIds) && count($checkedIds)) {
-            Post::whereIn('id', $checkedIds)->delete();
+            $user = BackendAuth::getUser();
+
+            foreach ($checkedIds as $postId) {
+                if (!$post = Post::find($postId))
+                    continue;
+
+                if($post->userCanEdit($user)) {
+                    $post->delete();
+                }
+            }
 
             Flash::success('Successfully deleted those posts.');
         }
 
         return $this->listRefresh();
+    }
+
+    public function update_onDelete($postId)
+    {
+        $user = BackendAuth::getUser();
+        $post = Post::find($postId);
+
+        if($post->userCanEdit($user)) {
+            return $this->asExtension('FormController')->update_onDelete($postId);
+        }
+
+        return \Redirect::to(Backend::url('rainlab/blog/posts'));
+    }
+
+    public function update_onSave($postId)
+    {
+        $user = BackendAuth::getUser();
+        $post = Post::find($postId);
+
+        if($post->userCanEdit($user)) {
+            return $this->asExtension('FormController')->update_onSave($postId);
+        }
+
+        return \Redirect::to(Backend::url('rainlab/blog/posts'));
     }
 
     /**
@@ -59,6 +119,11 @@ class Posts extends Controller
     {
         if (!$record->published)
             return 'safe disabled';
+    }
+
+    public function formBeforeCreate($model)
+    {
+        $model->user_id = BackendAuth::getUser()->id;
     }
 
     public function onRefreshPreview()
