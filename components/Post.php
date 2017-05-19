93a5f5336be5c3d115f52f990869a947e06bb35a
Pierre-André Vullioud <pa@inetis.ch>
2017-05-19 16:59:55 +0200

    Remove the isPublished scope for Admin

--- components/Post.php
+++ components/Post.php
@@ -2,111 +2,116 @@
 
 use Cms\Classes\Page;
 use Cms\Classes\ComponentBase;
+use Backend\Facades\BackendAuth;
 use RainLab\Blog\Models\Post as BlogPost;
 
 class Post extends ComponentBase
 {
-    /**
-     * @var RainLab\Blog\Models\Post The post model used for display.
-     */
-    public $post;
+	/**
+	 * @var RainLab\Blog\Models\Post The post model used for display.
+	 */
+	public $post;
 
-    /**
-     * @var string Reference to the page name for linking to categories.
-     */
-    public $categoryPage;
+	/**
+	 * @var string Reference to the page name for linking to categories.
+	 */
+	public $categoryPage;
 
-    public function componentDetails()
-    {
-        return [
-            'name'        => 'rainlab.blog::lang.settings.post_title',
-            'description' => 'rainlab.blog::lang.settings.post_description'
-        ];
-    }
+	public function componentDetails()
+	{
+		return [
+			'name'        => 'rainlab.blog::lang.settings.post_title',
+			'description' => 'rainlab.blog::lang.settings.post_description'
+		];
+	}
 
-    public function defineProperties()
-    {
-        return [
-            'slug' => [
-                'title'       => 'rainlab.blog::lang.settings.post_slug',
-                'description' => 'rainlab.blog::lang.settings.post_slug_description',
-                'default'     => '{{ :slug }}',
-                'type'        => 'string'
-            ],
-            'categoryPage' => [
-                'title'       => 'rainlab.blog::lang.settings.post_category',
-                'description' => 'rainlab.blog::lang.settings.post_category_description',
-                'type'        => 'dropdown',
-                'default'     => 'blog/category',
-            ],
-        ];
-    }
+	public function defineProperties()
+	{
+		return [
+			'slug' => [
+				'title'       => 'rainlab.blog::lang.settings.post_slug',
+				'description' => 'rainlab.blog::lang.settings.post_slug_description',
+				'default'     => '{{ :slug }}',
+				'type'        => 'string'
+			],
+			'categoryPage' => [
+				'title'       => 'rainlab.blog::lang.settings.post_category',
+				'description' => 'rainlab.blog::lang.settings.post_category_description',
+				'type'        => 'dropdown',
+				'default'     => 'blog/category',
+			],
+		];
+	}
 
-    public function getCategoryPageOptions()
-    {
-        return Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
-    }
+	public function getCategoryPageOptions()
+	{
+		return Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+	}
 
-    public function onRun()
-    {
-        $this->categoryPage = $this->page['categoryPage'] = $this->property('categoryPage');
-        $this->post = $this->page['post'] = $this->loadPost();
-    }
+	public function onRun()
+	{
+		$this->categoryPage = $this->page['categoryPage'] = $this->property('categoryPage');
+		$this->post = $this->page['post'] = $this->loadPost();
+	}
 
-    protected function loadPost()
-    {
-        $slug = $this->property('slug');
+	protected function loadPost()
+	{
+		$slug = $this->property('slug');
 
-        $post = new BlogPost;
+		$post = new BlogPost;
 
-        $post = $post->isClassExtendedWith('RainLab.Translate.Behaviors.TranslatableModel')
-            ? $post->transWhere('slug', $slug)
-            : $post->where('slug', $slug);
+		$post = $post->isClassExtendedWith('RainLab.Translate.Behaviors.TranslatableModel')
+			? $post->transWhere('slug', $slug)
+			: $post->where('slug', $slug);
 
-        $post = $post->isPublished()->first();
+		if(!BackendAuth::check())
+		{
+			$post = $post->isPublished();
+		}
+		$post = $post->first();
 
-        /*
-         * Add a "url" helper attribute for linking to each category
-         */
-        if ($post && $post->categories->count()) {
-            $post->categories->each(function($category) {
-                $category->setUrl($this->categoryPage, $this->controller);
-            });
-        }
+		/*
+		 * Add a "url" helper attribute for linking to each category
+		 */
+		if ($post && $post->categories->count()) {
+			$post->categories->each(function($category) {
+				$category->setUrl($this->categoryPage, $this->controller);
+			});
+		}
 
-        return $post;
-    }
+		return $post;
+	}
 
-    public function previousPost()
-    {
-        return $this->getPostSibling(-1);
-    }
+	public function previousPost()
+	{
+		return $this->getPostSibling(-1);
+	}
 
-    public function nextPost()
-    {
-        return $this->getPostSibling(1);
-    }
+	public function nextPost()
+	{
+		return $this->getPostSibling(1);
+	}
 
-    protected function getPostSibling($direction = 1)
-    {
-        if (!$this->post) {
-            return;
-        }
+	protected function getPostSibling($direction = 1)
+	{
+		if (!$this->post) {
+			return;
+		}
 
-        $method = $direction === -1 ? 'previousPost' : 'nextPost';
+		$method = $direction === -1 ? 'previousPost' : 'nextPost';
 
-        if (!$post = $this->post->$method()) {
-            return;
-        }
+		if (!$post = $this->post->$method()) {
+			return;
+		}
 
-        $postPage = $this->getPage()->getBaseFileName();
+		$postPage = $this->getPage()->getBaseFileName();
 
-        $post->setUrl($postPage, $this->controller);
+		$post->setUrl($postPage, $this->controller);
 
-        $post->categories->each(function($category) {
-            $category->setUrl($this->categoryPage, $this->controller);
-        });
+		$post->categories->each(function($category) {
+			$category->setUrl($this->categoryPage, $this->controller);
+		});
 
-        return $post;
-    }
+		return $post;
+	}
 }
