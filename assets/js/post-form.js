5c14df6cb2d2eb1d40c001ecb22ec0c838d8e0cc
Sam Geo <sam@daftspunk.com>
2021-08-04 11:39:06 +1000

    Back-end styling clean-up (#560)

--- assets/js/post-form.js
+++ assets/js/post-form.js
@@ -159,6 +159,7 @@
 
     PostForm.prototype.initLayout = function() {
         $('#Form-secondaryTabs .tab-pane.layout-cell:not(:first-child)').addClass('padded-pane')
+        $('#Form-secondaryTabs .nav-tabs > li:not(:first-child)').addClass('tab-content-bg')
     }
 
     PostForm.prototype.replacePlaceholder = function(placeholder, placeholderHtmlReplacement, mdCodePlaceholder, mdCodeReplacement) {
