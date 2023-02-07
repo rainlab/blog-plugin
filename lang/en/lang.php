c71b7b12bcc538bc461d72b6ae86f1a731d509b7
Jan Vince <jan@vince.cz>
2023-02-07 09:04:29 +0100

    Test richeditor in ML

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -28,6 +28,8 @@ return [
         'show_all_posts_comment' => 'Display both published and unpublished posts on the frontend to backend users',
         'use_legacy_editor_label' => 'Use the Legacy Markdown Editor',
         'use_legacy_editor_comment' => 'Enable the older version of the markdown editor when using October CMS v2 and above',
+        'use_richeditor_editor_label' => 'Use the WYSIWYG editor',
+        'use_richeditor_editor_comment' => 'Enable the richtext editor instead of the Markdown editor',
         'preview_cms_page_label' => 'Preview CMS Page',
         'preview_cms_page_comment' => 'Select a page to open for the Preview button',
         'tab_general' => 'General',
